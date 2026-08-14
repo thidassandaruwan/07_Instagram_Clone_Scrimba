7f2c021456e552faa9e2dcbc1b4708182e526336
thidassandaruwan <170103578+thidassandaruwan@users.noreply.github.com>
2026-08-14 11:40:55 +0530

    Implement dynamic post loading

---
+++ 01_Instagram/script.js
@@ -0,0 +1,47 @@
+import { posts } from "./posts.js";
+
+const feed = document.querySelector("#feed");
+
+function loadPosts(){
+    const postElements = [];
+    for (const post of posts){
+        postElements.push(createPostElement(post));
+    }
+    feed.innerHTML = postElements.join("");
+}
+
+function createPostElement(post){
+    return`
+        <article class="post">
+            <header class="post-header">
+                <img class="profile-pic" src="${post.avatar}" alt="${post.name}">
+                <div class="user-info">
+                    <span class="user-name">${post.name}</span>
+                    <span class="user-location">${post.location}</span>
+                </div>
+            </header>
+            <figure>
+                <img class="post-image" src="${post.post}" alt="wavy drawing of ${post.name}">
+            </figure>
+            <footer class="post-footer">
+                <div class="post-actions">
+                    <button class="btn-icon" aria-label="Like post">
+                        <img src="images/icon-heart.png" alt="" aria-hidden="true">
+                    </button>
+                    <button class="btn-icon" aria-label="Comment on post">
+                        <img src="images/icon-comment.png" alt="" aria-hidden="true">
+                    </button>   
+                    <button class="btn-icon" aria-label="Share post">
+                        <img src="images/icon-dm.png" alt="" aria-hidden="true">
+                    </button>   
+                </div>
+                <p class="like-count"><strong>${post.likes} likes</strong></p>
+                <p class="post-caption">
+                    <strong>${post.username}</strong> just took a few mushrooms lol.
+                </p>
+            </footer>
+        </article>
+    `;
+}
+
+loadPosts()
